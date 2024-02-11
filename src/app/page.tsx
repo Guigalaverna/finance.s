c5f16585416c71db44bce2f3ab330a31ee755c1a
Guilherme Galaverna <guigalaverna@gmail.com>
2024-02-10 23:29:34 -0300

    feat: create header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main>
-      <h1>asd</h1>
+      <header className="flex items-center justify-between">
+        <div className="flex items-center gap-5">
+          <span className="font-medium">finance.s</span>
+          <nav>
+            <Button variant="link">Home</Button>
+            <Button variant="link">Transações</Button>
+          </nav>
+        </div>
+        <Avatar>
+          <AvatarImage src="https://github.com/Guigalaverna.png" />
+          <AvatarFallback>GG</AvatarFallback>
+        </Avatar>
+      </header>
     </main>
   );
 }
